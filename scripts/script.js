1d599daacf783f2fb0ba94c55ef8bd3b4e782d50
Stanislav Kiselev <stenly1988@yandex.ru>
2023-02-18 19:37:42 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,19 @@ let buttonEdit = document.querySelector('.profile__button_type_edit'); // пер
 let buttonClose = document.querySelector('.popup__close-button'); // переменная - кнопка закрытия попапа
 let nameInput = document.querySelector('.popup__input_type_name'); // переменная ввода имени
 let hobbyInput = document.querySelector('.popup__input_type_hobby'); // переменная ввода хобби
-let getName = document.querySelector('.profile__name'); // переменная имени
-let getHobby = document.querySelector('.profile__hobby'); // переменная хобби
+let nameInProfile = document.querySelector('.profile__name'); // переменная имени
+let hobbyInProfile = document.querySelector('.profile__hobby'); // переменная хобби
 let formElement = document.querySelector('.popup__form'); // переменная формы
 
 buttonEdit.addEventListener('click', function () {
-  console.log(popup);
-  popup.classList.add('popup_opened');
+  openPopup(popup);
+  nameInput.value = nameInProfile.textContent;
+  hobbyInput.value = hobbyInProfile.textContent;
 })
 
 buttonClose.addEventListener('click', function () {
-  console.log(popup);
-  popup.classList.remove('popup_opened');
-})
+  closePopup(popup);
+  })
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -25,18 +25,10 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-
-let userName = getName.textContent;
-let userHobby = getHobby.textContent;
-
-nameInput.value = userName;
-hobbyInput.value = userHobby;
-
-
 function formSubmitHandler (evt) {
-	evt.preventDefault(); 
-	getName.textContent = nameInput.value;
-	getHobby.textContent = hobbyInput.value;
+  evt.preventDefault();
+  nameInProfile.textContent = nameInput.value;
+  hobbyInProfile.textContent = hobbyInput.value;
   closePopup(popup);
 }
 
