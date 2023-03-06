ade95f383b6d2ecd917f5fd1176529d899356126
Stanislav Kiselev <stenly1988@yandex.ru>
2023-03-06 13:52:37 +0300

    fix let and function

--- scripts/script.js
+++ scripts/script.js
@@ -1,149 +1,147 @@
-let popup = document.querySelector(".popup"); // переменная попапа
-let buttonEdit = document.querySelector(".profile__button_type_edit"); // переменная - кнопка редактирования профиля
-let buttonClose = document.querySelectorAll(".popup__close-button"); // переменная - кнопка закрытия попапа
-let nameInput = document.querySelector(".popup__input_type_name"); // переменная ввода имени
-let hobbyInput = document.querySelector(".popup__input_type_hobby"); // переменная ввода хобби
-let nameInProfile = document.querySelector(".profile__name"); // переменная имени
-let hobbyInProfile = document.querySelector(".profile__hobby"); // переменная хобби
-let formElement = document.querySelector(".popup__form"); // переменная формы
+//let popup = document.querySelector(".popup"); // переменная попапа
+const profilePopup = document.querySelector('.popup_type_profile'); // переменная попапа профиля
+const buttonEdit = document.querySelector('.profile__button_type_edit'); // переменная - кнопка редактирования профиля
+const closeButtons = document.querySelectorAll('.popup__close-button'); // переменная - кнопки закрытия попапов
+const nameInput = document.querySelector('.popup__input_type_name'); // переменная ввода имени
+const hobbyInput = document.querySelector('.popup__input_type_hobby'); // переменная ввода хобби
+const nameInProfile = document.querySelector('.profile__name'); // переменная имени
+const hobbyInProfile = document.querySelector('.profile__hobby'); // переменная хобби
+const profileForm = document.getElementsByName('profile edit')[0]; // переменная формы профиля(по имени формы)
 
-buttonEdit.addEventListener("click", function () {
-  openPopup(popup);
+buttonEdit.addEventListener('click', function () {
+  openPopup(profilePopup);
   nameInput.value = nameInProfile.textContent;
   hobbyInput.value = hobbyInProfile.textContent;
 });
 
-buttonClose.forEach(function (button) {
-  button.addEventListener("click", () => {
-    closePopup(popup);
-    closePopup(popupAddCard);
-    closePopup(imageZoom);
-  });
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
 });
 
 function openPopup(popup) {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function closePopup(popup) {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   nameInProfile.textContent = nameInput.value;
   hobbyInProfile.textContent = hobbyInput.value;
-  closePopup(popup);
+  closePopup(profilePopup);
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
+profileForm.addEventListener('submit', handleProfileFormSubmit);
 // 5 ПР
-let buttonAdd = document.querySelector(".profile__button_type_add"); // переменная - кнопкa добавления карточек
-let popupAddCard = document.querySelector(".popup_type_add-card"); // переменная попапа добавления карточек
+const buttonAddCard = document.querySelector('.profile__button_type_add'); // переменная - кнопкa добавления карточек
+const popupAddCard = document.querySelector('.popup_type_add-card'); // переменная попапа добавления карточек
 
-buttonAdd.addEventListener("click", function () {
+buttonAddCard.addEventListener('click', function () {
   openPopup(popupAddCard);
 });
 
 const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
   },
   {
-    name: "Камчатка",
-    link: "https://images.unsplash.com/photo-1665073140507-0bad3d962476?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=903&q=80",
+    name: 'Камчатка',
+    link: 'https://images.unsplash.com/photo-1665073140507-0bad3d962476?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=903&q=80',
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
   },
   {
-    name: "Сахалин",
-    link: "https://images.unsplash.com/photo-1662953748980-f8adec7fdf6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    name: 'Сахалин',
+    link: 'https://images.unsplash.com/photo-1662953748980-f8adec7fdf6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
   },
   {
-    name: "Владимир",
-    link: "./images/mesto/Vladimir.jpg",
+    name: 'Владимир',
+    link: './images/mesto/Vladimir.jpg',
   },
   {
-    name: "Ростов",
-    link: "./images/mesto/Rostov.jpg",
+    name: 'Ростов',
+    link: './images/mesto/Rostov.jpg',
   },
   {
-    name: "Гора Эльбрус",
-    link: "./images/mesto/Gora_Elbrus.jpg",
+    name: 'Гора Эльбрус',
+    link: './images/mesto/Gora_Elbrus.jpg',
   },
   {
-    name: "Домбай",
-    link: "./images/mesto/Dombai.jpg",
+    name: 'Домбай',
+    link: './images/mesto/Dombai.jpg',
   },
   {
-    name: "Карачаевск",
-    link: "./images/mesto/Kara4aevsk.png",
+    name: 'Карачаевск',
+    link: './images/mesto/Kara4aevsk.png',
   },
   {
-    name: "Байкал",
-    link: "./images/mesto/Baikal.jpg",
+    name: 'Байкал',
+    link: './images/mesto/Baikal.jpg',
   },
 ];
 
-const cards = document.querySelector(".cards");
+const cards = document.querySelector('.cards');
 
 function createCard(card) {
-  const newCard = document.querySelector("#cardTemplate").content.cloneNode(true);
-  const cardTitle = newCard.querySelector(".card__title");
+  const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
+  const cardTitle = newCard.querySelector('.card__title');
   cardTitle.textContent = card.name;
-  const cardImage = newCard.querySelector(".card__image");
+  const cardImage = newCard.querySelector('.card__image');
   cardImage.src = card.link;
   cardImage.alt = card.name;
+  const buttonDeletCard = newCard.querySelector('.card__image-delet-button');
+  buttonDeletCard.addEventListener('click', deleteCard);
+  const buttonLike = newCard.querySelector('.card__like-button');
   cards.prepend(newCard);
-  const buttonDeletCard = document.querySelector(".card__image-delet-button");
-  buttonDeletCard.addEventListener("click", deletingCard);
-  const buttonLike = document.querySelector(".card__like-button");
-  buttonLike.addEventListener("click", AddLike);
-  cardImage.addEventListener("click", function () {
+  buttonLike.addEventListener('click', аddLike);
+  cardImage.addEventListener('click', function () {
     openPopupZoomImage(card);
   });
 }
 
 initialCards.forEach(createCard);
 
-function deletingCard(event) {
+function deleteCard(event) {
   const button = event.target;
-  const card = button.closest(".card");
+  const card = button.closest('.card');
   card.remove();
 }
 
-function AddLike(event) {
+function аddLike(event) {
   const button = event.target;
-  button.classList.toggle("card__like-button_type_active");
+  button.classList.toggle('card__like-button_type_active');
 }
 
-const formAddNewCard = popupAddCard.querySelector(".popup__form");
-formAddNewCard.addEventListener("submit", formSubmitHandlerImg);
-function formSubmitHandlerImg(event) {
+const newCardForm = document.getElementsByName('image edit')[0];
+newCardForm.addEventListener('submit', handleNewCardFormSubmit);
+function handleNewCardFormSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const link = form.querySelector(".popup__input_type_link-img").value;
-  const name = form.querySelector(".popup__input_type_name-img").value;
+  const link = form.querySelector('.popup__input_type_link-img').value;
+  const name = form.querySelector('.popup__input_type_name-img').value;
   const card = { name, link };
   createCard(card);
   closePopup(popupAddCard);
   form.reset();
 }
 
-let imageZoom = document.querySelector(".popup_type_zoom-image");
-let cardImagePopup = document.querySelector(".popup__image");
-let cardImageCaption = document.querySelector(".popup__image-caption");
+const imageZoom = document.querySelector('.popup_type_zoom-image');
+const cardImagePopup = document.querySelector('.popup__image');
+const cardImageCaption = document.querySelector('.popup__image-caption');
 
 function openPopupZoomImage(element) {
   cardImagePopup.src = element.link;
